29faae8bb92455d77f03fd59924c28392c7c9654
FrenDo0 <lubo_raykonen@mail.bg>
2022-03-10 17:51:46 +0200

    Add project files.

---
+++ BlackJackGameConsoleVersion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlackJackGameConsoleVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
