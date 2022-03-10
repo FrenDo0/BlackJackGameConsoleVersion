3704a68adcf8eeb9bff03c8e9f2052fcacbf9df8
FrenDo0 <lubo_raykonen@mail.bg>
2022-03-10 18:20:37 +0200

    Wrote some functions in Deck class.

--- BlackJackGameConsoleVersion/Deck.cs
+++ BlackJackGameConsoleVersion/Deck.cs
@@ -8,5 +8,22 @@ namespace BlackJackGameConsoleVersion
 {
     class Deck
     {
+        List<Cards> currentDeck;
+        int sizeOfDeck;
+
+        //You can say how many decks you wanna play with
+        // sizeOfDeck = 52 -> 1 deck
+        // sizeOfDeck = 104 -> 2 decks...
+        public Deck(int size)
+        {
+            sizeOfDeck = size;
+            currentDeck = new List<Cards>(sizeOfDeck);
+        }
+
+        public List<Cards> getDeck()
+        {
+            return currentDeck;
+        }
+
     }
 }
